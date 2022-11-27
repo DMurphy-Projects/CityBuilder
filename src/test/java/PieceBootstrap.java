e0b74929414f15ba438cacb86b9475ee0d0d9b58
Dean-PC\Dean <dmurphy1994@hotmail.co.uk>
2022-11-27 17:35:12 +0000

    Added a way of creating new city config files

---
+++ src/test/java/PieceBootstrap.java
@@ -0,0 +1,33 @@
+import io.TileXmlWriter;
+import model.CityTile;
+import tile.TileHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+
+public class PieceBootstrap {
+
+    public static void main(String[] args) throws IOException, TransformerException, ParserConfigurationException {
+        TileHandler tileHandler = new TileHandler(4);
+
+        CityTile[] tiles = new CityTile[]{
+                new CityTile("ROAD", 2, 4),
+                new CityTile("TURN", 4, 4),
+                new CityTile("JUNCTION", 4, 4),
+                new CityTile("INTERSECTION", 1, 4),
+                new CityTile("BLANK", 1, 4),
+                new CityTile("BUILDING", 1, 4),
+        };
+
+        for (CityTile tile: tiles)
+        {
+            //co-opting rotation function for the bootstrap
+            for (int i=0;i<4;i++) {
+                tileHandler.add(tile.getBaseId(), i, i % tile.getRotation());
+            }
+        }
+
+        TileXmlWriter.writeToFile("src/main/resources/cityTiles.xml", tileHandler);
+    }
+}
